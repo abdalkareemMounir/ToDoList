146699242a512ed399e4f2d6aaa547894b39d5cf
abdalkareem mounir <abdalkareem.m.2001@icloud.com>
2024-07-28 18:11:29 +0300

    test

--- functions.py
+++ functions.py
@@ -1,11 +1,12 @@
-def get_todos(filepath="Todo.txt"):
+file_path="Todo.txt"
+def get_todos(filepath=file_path):
     """read text file and return it's content"""
     with open(filepath, "r") as file_local:
             content_local = file_local.readlines()
     return content_local
 
 
-def set_todos(new_to_do,filepath="Todo.txt"):
+def set_todos(new_to_do,filepath=file_path):
     """take file path and what to write"""
     with open(filepath , "w") as file:
                 file.writelines(new_to_do)
