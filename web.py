1f941276844509e866fddd663142f4bf1735c75d
abdalkareem mounir <abdalkareem.m.2001@icloud.com>
2024-07-30 02:10:24 +0300

    todo app

--- web.py
+++ web.py
@@ -14,8 +14,15 @@ st.subheader("this is my todo app")
 
 st.write("this app increase your productivety")
 
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.set_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="",placeholder="Add new todo...",
               on_change=add_todo,key="new_todo")
+
+st.session_state
